63f19b9b5066f49686b9c5392be2991fadd45ec0
Vohiro <dvohiro@gmail.com>
2023-08-13 14:24:55 -0700

    first task

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """ prints all integers of a list."""
+    my_list = [1, 2, 3, 4, 5]
+    for my in my_list
+    print ("{}".format(my))
