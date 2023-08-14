c7486841f1a57e0ae933eacf2427dfcc4722f30d
Vohiro <dvohiro@gmail.com>
2023-08-14 02:03:06 -0700

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+def element_at(my_list, idx):
+    """ retrieves an element from a list"""
+    for my in range(len(my_list)):
+        if idx == my
+            print ("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    if idx < 0 or idx > my
+        return none
