2f59ae0d3b9df6e125ec957f2c72973dfc24624a
Vohiro <dvohiro@gmail.com>
2023-11-13 12:10:44 -0800

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const FirstLine = “C is fun”;
+console.log(FirstLine);
+const SecondLine = “Python is cool”;
+console.log(SecondLine);
+const ThirdLine = “JavaScript is amazing”;
+console.log(ThirdLine);
